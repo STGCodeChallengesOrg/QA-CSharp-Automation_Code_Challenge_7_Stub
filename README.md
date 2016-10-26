428c91dfe7c34c4fadf2fe35475047e2622cfca4
Richard Harkins <rharkins111@gmail.com>
2016-10-26 14:04:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+QA CSharp Automation Code Challenge #7 (Crawler and get text):
+
+Extension of last project.  Use the crawler and catalog every word used and the number of instance each word occurs.  
+
+Go to https://www.skiutah.com/
+
+use the crawler written in the past challenges and write a method to keep track of all the words on each page on the site and return to me a list or dictionary of words that exists on the site.  Also send me the list or dictionary in a text document.  
+map.put(key, map.get(key) + 1);
+
+The sample code stub is found in the Utils.java file.
+
+Refer to the Git Procedure Step By Step document, which contains detailed instructions for accessing code stubs and submitting completed work from/to GitHub.
---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+automation challenge 7 (Crawler and get text):
+Extension of last project.  Use the crawler and catalog every word used and the number of instance each word occurs.  
+Go to https://www.skiutah.com/
+
+use the crawler written in the past challenges and write a method to keep track of all the words on each page on the site and return to me a list or dictionary of words that exists on the site.  Also send me the list or dictionary in a text document.  
+map.put(key, map.get(key) + 1);
+
+*/
+namespace Automation_Code_Challenge_7_CSharp
+{
+    [TestClass]
+    class Utils : SeleniumWebdriverBaseClass
+    {
+        [TestMethod]
+        public void TestLauncher()
+        {
+            // Verify Page Title
+            verifyPageTitle(URL, Expected_Value);
+            // Visit every page of the skiutah.com domain once, compiling a list of all the words on each page.
+            // Return a list of all the words found, with the number of occurences for each word
+            webCrawler();
+        }
+    }
+}
